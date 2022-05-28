ee8b2c3527f6678f2dda247f314c7b473eaf60db
Cristian Camilo Arenas Castellanos <44928834+cristiancamiloarenas@users.noreply.github.com>
2022-05-28 17:42:48 -0500

    Solved DigitalOcean problem. Migrated to Amazon RDS

--- routes/index.js
+++ routes/index.js
@@ -5,21 +5,20 @@ router.use(bodyParser.json());
 const fs = require('fs');
 const {Client} = require('pg');
 
-const config = {
-    connectionString: 'postgresql://doadmin:AVNS_bqJr3p5s5vkC7YV@db-postgresql-nyc3-07925-do-user-11579649-0.b.db.ondigitalocean.com:25060/defaultdb',
-    // Beware! The ssl object is overwritten when parsing the connectionString
-    ssl: {
-        rejectUnauthorized: false,
-        ca: fs.readFileSync('routes/ca-certificate.crt.txt').toString(),
-    },
+const connectionData = {
+    user: 'postgres',
+    host: 'database-1.c19z1eoho6zx.us-east-1.rds.amazonaws.com',
+    database: 'postgres',
+    password: 'postgres',
+    port: 5432,
 }
-const client = new Client(config)
+
+const client = new Client(connectionData)
 client.connect();
 
 /* GET home page. */
 router.get('/readEmployees', (req, res) => {
-
-    client.query('SELECT * FROM Empleados', (err, clientResponse) => {
+    client.query('select * FROM Empleado', (err, clientResponse) => {
         console.log(err, clientResponse);
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
@@ -32,7 +31,7 @@ router.get('/readEmployees', (req, res) => {
 })
 
 router.post("/createEmployee", (req, res) => {
-    const query = 'INSERT INTO Empleados (id,nombre,apellido,edad,sexo) VALUES($1, $2, $3, $4, $5)';
+    const query = 'INSERT INTO Empleado (id,nombre,apellido,edad,sexo) VALUES($1, $2, $3, $4, $5)';
     const values = [req.body.id,req.body.nombre,req.body.apellido,req.body.edad,req.body.sexo];
 
     client.query(query,values, (err, clientResponse) => {
@@ -46,7 +45,7 @@ router.post("/createEmployee", (req, res) => {
 })
 
 router.put("/updateEmployee", (req, res) => {
-    const query = 'UPDATE Empleados SET nombre=$2,apellido=$3,edad=$4, sexo=$5 WHERE id = $1';
+    const query = 'UPDATE Empleado SET nombre=$2,apellido=$3,edad=$4, sexo=$5 WHERE id = $1';
     const values = [req.body.id,req.body.nombre,req.body.apellido,req.body.edad,req.body.sexo];
 
     client.query(query,values, (err, clientResponse) => {
@@ -60,7 +59,7 @@ router.put("/updateEmployee", (req, res) => {
 })
 
 router.delete("/deleteEmployee", (req, res) => {
-    const query = 'DELETE FROM Empleados WHERE id= $1';
+    const query = 'DELETE FROM Empleado WHERE id= $1';
     const values = [req.body.id];
 
     client.query(query,values, (err, clientResponse) => {
